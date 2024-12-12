c27b0db29f502cf91c958c0ec535fca6b5cb24cb
unknown <akinrataa@gmail.com>
2024-12-12 18:05:04 +0100

    update on file

--- src/Component/Evaluation.jsx
+++ src/Component/Evaluation.jsx
@@ -1,18 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { FiFilePlus } from "react-icons/fi";
 import { CgFolderAdd } from "react-icons/cg";
-import Folder from './Folder';
+import Folder from "./Folder";
 
 const Evaluation = () => {
     const [expandedFolders, setExpandedFolders] = useState([]);
     const [editing, setEditing] = useState(null);
     const [editValue, setEditValue] = useState("");
     const [focusedItem, setFocusedItem] = useState(null);
-    const [data,setData] =useState(null)
+
+    // Initial State
+    const [data, setData] = useState([
+        {
+            id: "1",
+            name: "Documents",
+            type: "folder",
+            files: ["Document1.jpg", "Document2.jpg", "Document3.jpg"],
+        },
+        {
+            id: "2",
+            name: "Desktop",
+            type: "folder",
+            files: ["Screenshot1.jpg", "videopal.mp4"],
+        },
+        {
+            id: "3",
+            name: "Downloads",
+            type: "folder",
+            files: [
+                {
+                    id: "4",
+                    name: "Drivers",
+                    type: "folder",
+                    files: ["Printerdriver.dmg", "cameradriver.dmg"],
+                },
+                "chromedriver.dmg",
+            ],
+        },
+        {
+            id: "5",
+            name: "Applications",
+            type: "folder",
+            files: [
+                "Webstorm.dmg",
+                "Pycharm.dmg",
+                "FileZila.dmg",
+                "Mattermost.dmg",
+            ],
+        },
+    ]);
 
     const toggleExpand = (id) => {
         setExpandedFolders((prev) =>
-            prev.includes(id) ? prev.filter((folderId) => folderId !== id) : [...prev, id]
+            prev.includes(id)
+                ? prev.filter((folderId) => folderId !== id)
+                : [...prev, id]
         );
     };
 
@@ -27,59 +69,83 @@ const Evaluation = () => {
         setFocusedItem(id);
     };
 
+    const addFolder = () => {
+        const folderName = prompt("Enter new folder name:");
+        if (folderName) {
+            setData((prevData) => [
+                ...prevData,
+                { id: Date.now().toString(), name: folderName, type: "folder", files: [] },
+            ]);
+        }
+    };
 
-    useEffect(()=>{
-        fetch('http://localhost:8000/folders')
-        .then(res=>{
-            return res.json();
-        }).then(data=>{
-            console.log(data)
-            setData(data)
-        })
-    },[])
-    // const data = [
-    //     {
-    //         id: 1,
-    //         name: 'Desktop',
-    //         type: 'folder',
-    //         files: ['screenshot.png', 'document.docx']
-    //     },
-    //     {
-    //         id: 2,
-    //         name: 'Downloads',
-    //         type: 'folder',
-    //         files: ['image.jpg', 'video.mp4']
-    //     }
-    // ];
+    const addFile = (folderId) => {
+        if (!folderId) {
+            alert("Please select a folder to add the file.");
+            return;
+        }
+
+        const folder = data.find((item) => item.id === folderId);
+
+        if (folder && folder.type === "folder") {
+            
+            const fileName = prompt("Enter new file name:");
+            if (fileName) {
+                setData((prevData) =>
+                    prevData.map((item) =>
+                        item.id === folderId
+                            ? { ...item, files: [...item.files, fileName] }
+                            : item
+                    )
+                );
+            }
+        } else {
+            alert("Please select a valid folder to add the file.");
+        }
+    };
 
     return (
-        <div className='bg-gray-800 text-gray-200 p-4 w-full h-screen'>
-            <div className='flex justify-between items-center p-2'>
-                <div className='flex items-center'>
-                    <h1 className='uppercase px-2'>Evaluation</h1>
+        <div className="bg-gray-800 text-gray-200 p-4 w-full h-screen">
+            <div className="flex justify-between items-center p-2">
+                <div className="flex items-center">
+                    <h1 className="uppercase px-2">Evaluation</h1>
                 </div>
-                <div className='flex'>
-                    <span className='px-2 cursor-pointer' title='Add File'><FiFilePlus size={25} /></span>
-                    <span className='cursor-pointer' title='Add Folder'><CgFolderAdd size={25} /></span>
+                <div className="flex">
+                    <span
+                        className="px-2 cursor-pointer"
+                        title="Add File"
+                        onClick={() => addFile(focusedItem)}
+                    >
+                        <FiFilePlus size={25} />
+                    </span>
+
+                    <span
+                        className="cursor-pointer"
+                        title="Add Folder"
+                        onClick={addFolder}
+                    >
+                        <CgFolderAdd size={25} />
+                    </span>
                 </div>
             </div>
-            <div className='border-t-[1px] border-l-[1px] w-full h-screen border-gray-500'>
-                {data&&data.map((folder) => (
-                    <Folder
-                        key={folder.id}
-                        folder={folder}
-                        isExpanded={expandedFolders.includes(folder.id)}
-                        focusedItem={focusedItem}
-                        onExpand={toggleExpand}
-                        onEdit={() => {}}
-                        onFocus={handleFocus}
-                        editing={editing}
-                        editValue={editValue}
-                        setEditing={setEditing}
-                        setEditValue={setEditValue}
-                        saveEdit={saveEdit}
-                    />
-                ))}
+            <div className="border-t-[1px] border-l-[1px] w-full h-screen border-gray-500">
+                {data &&
+                    data.map((folder) => (
+                        <Folder
+                            key={folder.id}
+                            folder={folder}
+                            isExpanded={expandedFolders.includes(folder.id)}
+                            focusedItem={focusedItem}
+                            onExpand={toggleExpand}
+                            onEdit={() => {}}
+                            onFocus={handleFocus}
+                            editing={editing}
+                            editValue={editValue}
+                            setEditing={setEditing}
+                            setEditValue={setEditValue}
+                            saveEdit={saveEdit}
+                        />
+                    ))}
             </div>
         </div>
     );
--- src/Component/Folder.jsx
+++ src/Component/Folder.jsx
@@ -2,55 +2,100 @@ import React from 'react';
 import { FaAngleDown, FaAngleRight } from "react-icons/fa6";
 import File from './File';
 
-const Folder = ({ folder, isExpanded, focusedItem, onExpand, onEdit, onFocus, editing, editValue, setEditing, setEditValue, saveEdit }) => {
+const Folder = ({
+    folder,
+    isExpanded,
+    focusedItem,
+    onExpand,
+    onEdit,
+    onFocus,
+    editing,
+    editValue,
+    setEditing,
+    setEditValue,
+    saveEdit,
+}) => {
+    const handleDoubleClick = () => {
+        setEditing({ type: 'folder', id: folder.id });
+        setEditValue(folder.name);
+    };
+
+    const handleBlurOrEnter = (e) => {
+        if (e.type === 'blur' || e.key === 'Enter') {
+            saveEdit();
+        }
+    };
+
     return (
-        <div className='px-4'>
+        <div className="px-4">
+            {/* Folder Header */}
             <div
                 onClick={() => {
                     onExpand(folder.id);
                     onFocus(folder.id);
                 }}
-                onDoubleClick={() => {
-                    setEditing({ type: 'folder', id: folder.id });
-                    setEditValue(folder.name);
-                }}
+                onDoubleClick={handleDoubleClick}
                 className={`flex items-center p-1 hover:bg-gray-700 hover:text-white cursor-pointer 
-                            ${focusedItem === folder.id ? 'bg-gray-600' : ''}`}
+                    ${focusedItem === folder.id ? 'bg-gray-600' : ''}`}
             >
+                {/* Expand/Collapse Icon */}
                 <span className="cursor-pointer">
                     {isExpanded ? <FaAngleDown size={20} /> : <FaAngleRight size={20} />}
                 </span>
+
+                {/* Folder Name or Editable Input */}
                 {editing?.type === 'folder' && editing.id === folder.id ? (
                     <input
                         type="text"
                         value={editValue}
                         onChange={(e) => setEditValue(e.target.value)}
-                        onBlur={saveEdit}
-                        onKeyDown={(e) => e.key === 'Enter' && saveEdit()}
+                        onBlur={handleBlurOrEnter}
+                        onKeyDown={handleBlurOrEnter}
                         autoFocus
                         className="ml-2 text-black px-1"
                     />
                 ) : (
-                    <h1 className='uppercase px-2'>{folder.name}</h1>
+                    <h1 className="uppercase px-2">{folder.name}</h1>
                 )}
             </div>
+
+            {/* Render Folder Contents */}
             {isExpanded &&
-                folder.files.map((file, index) => (
-                    <File
-                        key={index}
-                        file={file.name}
-                        folderId={folder.id}
-                        index={index}
-                        focusedItem={focusedItem}
-                        onEdit={onEdit}
-                        onFocus={onFocus}
-                        editing={editing}
-                        editValue={editValue}
-                        setEditing={setEditing}
-                        setEditValue={setEditValue}
-                        saveEdit={saveEdit}
-                    />
-                ))}
+                folder.files.map((file, index) =>
+                    typeof file === 'object' && file.type === 'folder' ? (
+                        // Recursive Folder Component for Nested Folders
+                        <Folder
+                            key={file.id}
+                            folder={file}
+                            isExpanded={isExpanded}
+                            focusedItem={focusedItem}
+                            onExpand={onExpand}
+                            onEdit={onEdit}
+                            onFocus={onFocus}
+                            editing={editing}
+                            editValue={editValue}
+                            setEditing={setEditing}
+                            setEditValue={setEditValue}
+                            saveEdit={saveEdit}
+                        />
+                    ) : (
+                        // File Component for Files
+                        <File
+                            key={index}
+                            file={file}
+                            folderId={folder.id}
+                            index={index}
+                            focusedItem={focusedItem}
+                            onEdit={onEdit}
+                            onFocus={onFocus}
+                            editing={editing}
+                            editValue={editValue}
+                            setEditing={setEditing}
+                            setEditValue={setEditValue}
+                            saveEdit={saveEdit}
+                        />
+                    )
+                )}
         </div>
     );
 };
